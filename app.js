b937c1c8e254e003ebf75e3e12c97ca33ea3089c
JamieNicholson <jamienicholson06@outlook.com>
2024-07-19 13:27:21 +0100

    Update app.js

--- app.js
+++ app.js
@@ -12,10 +12,10 @@ async function uploadFile() {
     const content = e.target.result;
     const base64Content = btoa(content);
 
-    const getShaResponse = await fetch('https://api.github.com/repos/YOUR_USERNAME/YOUR_REPO/contents/PATH_TO_FILE.xlsx', {
+    const getShaResponse = await fetch('https://api.github.com/repos/JamieNicholson/Part-finder/contents/excel.xlsx', {
       method: 'GET',
       headers: {
-        'Authorization': 'token YOUR_GITHUB_TOKEN',
+        'Authorization': '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',
         'Accept': 'application/vnd.github.v3+json'
       }
     });
@@ -28,10 +28,10 @@ async function uploadFile() {
     const fileData = await getShaResponse.json();
     const sha = fileData.sha;
 
-    const response = await fetch('https://api.github.com/repos/YOUR_USERNAME/YOUR_REPO/contents/PATH_TO_FILE.xlsx', {
+    const response = await fetch('https://api.github.com/repos/JamieNicholson/Part-finder/contents/excel.xlsx', {
       method: 'PUT',
       headers: {
-        'Authorization': 'token YOUR_GITHUB_TOKEN',
+        'Authorization': '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',
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
